6c504993ed2abbcb8e1c60d9da9ceda9548a193f
CO2NoTear <sqede@outlook.com>
2023-08-31 22:47:44 +0800

    first commit

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,17 @@
+local keymap = vim.keymap
+
+vim.g.mapleader = "\\"
+
+-- Insertation mode --
+
+
+
+-- Visual mode --
+-- keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- no highlight
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- nvim-tree
+keymap.set("n", "<F3>", ":NvimTreeToggle<CR>")
